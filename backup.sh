15ada4e09a63dc09499b86cf74f8bd3c7ee59aaa
Dovydas Venckus <dovydas.venckus@live.com>
2015-09-28 22:26:44 +0300

    Made function for logging. Reduced boilerplate code

--- backup.sh
+++ backup.sh
@@ -4,28 +4,29 @@ SERVICES="cron tomcat7 deluge-daemon samba rsync postgresql"
 IMAGE_NAME='raspberry-backup'
 BACKUP_DATE=$(date "+%Y-%m-%d")
 IMAGE_FILE_NAME="$BACKUP_PATH$IMAGE_NAME-$BACKUP_DATE.img"
-CURRENT_TIME=$(date "+%Y-%m-%d %H:%M:%S")
-echo "$CURRENT_TIME Started backup process"
+
+log(){
+  CURRENT_TIME=$(date "+%Y-%m-%d %H:%M:%S")
+  echo "$CURRENT_TIME $1"
+}
+
+log "Started backup process"
 
 for service in $SERVICES
 do
-  echo "Stopping $service..."
+  log "Stopping $service..."
   /etc/init.d/$service stop
 done
 
-IMAGING_START_TIME=$(date "+%Y-%m-%d %H:%M:%S")
-echo "$IMAGING_START_TIME Image creation started"
+log "Image creation started"
 
 dd if=/dev/mmcblk0 of=$IMAGE_FILE_NAME
 
-IMAGE_COMPLETED_TIME=$(date "+%Y-%m-%d %H:%M:%S")
-echo "$IMAGE_COMPLETED_TIME Image successfully created"
+log "Image successfully created"
 
 for service in $SERVICES
 do
-  echo "Starting $service..."
+  log "Starting $service..."
   /etc/init.d/$service start
 done
 
-SCRIPT_COMPLETE_TIME=$(date "+%Y-%m-%d %H:%M:%S")
-echo "$SCRIPT_COMPLETE_TIME Backup process was successfull"
